9ae2f5dfa95f57b2db0d90a7ca7ed7973560c7d6
GuillaumeP <guillaume.pani@gmail.com>
2018-10-09 15:48:51 +0200

    classe partielle pour régler le soucis de boucle lors de GetApi

---
+++ WebApi/WebApi/Models/Bdd_Partial/Question.override.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using WebApi.Models.Bdd;
+
+namespace WebApi.Models.Bdd_Partial
+{
+    [MetadataType(typeof(QuestionMetaData))]
+    public partial class Question
+    {
+    }
+
+    public class QuestionMetaData
+    {
+        [JsonIgnore]
+        [IgnoreDataMember]
+        public virtual ICollection<Composant> Composant { get; set; }
+    }
+}
