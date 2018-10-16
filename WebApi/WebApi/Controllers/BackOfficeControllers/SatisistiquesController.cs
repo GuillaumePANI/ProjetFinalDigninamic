5901e935c8824265a729d045e0ecf0770a7543b5
GuillaumeP <guillaume.pani@gmail.com>
2018-10-16 11:45:57 +0200

    commit statistiques back office

---
+++ WebApi/WebApi/Controllers/BackOfficeControllers/SatisistiquesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApi.Models;
+using WebApi.Models.Bdd;
+using WebApi.Repository;
+
+namespace WebApi.Controllers.BackOfficeControllers
+{
+    public class StatistiquesController : Controller
+    {
+        private readonly FormulaireRepository formRepo = new FormulaireRepository();
+        private readonly StatistiqueRepository statRepo = new StatistiqueRepository();
+
+        // GET: Satisistiques
+        public ActionResult Index()
+        {
+            List<Formulaire> formulairesClos = formRepo.GetAllFormulaires().Where(f => f.dateCloturation < DateTime.Now).ToList();
+
+            return View(formulairesClos);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            List<Statistique> statsFormulaire = statRepo.GetStatistiqueByFormulaire((int)id).ToList();
+            if (statsFormulaire == null)
+            {
+                return HttpNotFound();
+            }
+            return View(statsFormulaire);
+
+        }
+    }
+}
